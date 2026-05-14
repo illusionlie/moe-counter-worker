0806bd9de0ec737e9a39e4a391cf5e3149b6c146
IllusionLie <153069291+illusionlie@users.noreply.github.com>
2026-05-15 00:10:13 +0800

    migrate themes

--- themes/data.js
+++ themes/data.js
@@ -1,17 +1,115 @@
+import _3dNum from './3d-num/index.js';
+import ai1 from './ai-1/index.js';
 import asoul from './asoul/index.js';
+import booruFfsr from './booru-ffsr/index.js';
+import booruHelltaker from './booru-helltaker/index.js';
+import booruHuggboo from './booru-huggboo/index.js';
+import booruJaypee from './booru-jaypee/index.js';
+import booruKoe from './booru-koe/index.js';
+import booruLewd from './booru-lewd/index.js';
+import booruLisu from './booru-lisu/index.js';
+import booruMjg from './booru-mjg/index.js';
+import booruMof from './booru-mof/index.js';
+import booruNandroid from './booru-nandroid/index.js';
+import booruQualityhentais from './booru-qualityhentais/index.js';
+import booruR6gdrawfriends from './booru-r6gdrawfriends/index.js';
+import booruRfck from './booru-rfck/index.js';
+import booruSmtg from './booru-smtg/index.js';
+import booruSnyde from './booru-snyde/index.js';
+import booruTheCollection from './booru-the-collection/index.js';
+import booruTouhoulat from './booru-touhoulat/index.js';
+import booruTownofgravityfalls from './booru-townofgravityfalls/index.js';
+import booruTwifanartsfw from './booru-twifanartsfw/index.js';
+import booruVe from './booru-ve/index.js';
+import booruVivi from './booru-vivi/index.js';
+import booruVp from './booru-vp/index.js';
+import booruYuyuyui from './booru-yuyuyui/index.js';
+import capoo1 from './capoo-1/index.js';
+import capoo2 from './capoo-2/index.js';
+import e621 from './e621/index.js';
+import food from './food/index.js';
 import gelbooru from './gelbooru/index.js';
 import gelbooruH from './gelbooru-h/index.js';
+import green from './green/index.js';
+import kasuterura1 from './kasuterura-1/index.js';
+import kasuterura2 from './kasuterura-2/index.js';
+import kasuterura3 from './kasuterura-3/index.js';
+import kasuterura4 from './kasuterura-4/index.js';
+import kyun from './kyun/index.js';
+import loveAndDeepspace from './love-and-deepspace/index.js';
+import miku from './miku/index.js';
+import minecraft from './minecraft/index.js';
 import moebooru from './moebooru/index.js';
 import moebooruH from './moebooru-h/index.js';
+import mordenNum from './morden-num/index.js';
+import nixietube1 from './nixietube-1/index.js';
+import nixietube2 from './nixietube-2/index.js';
+import normal1 from './normal-1/index.js';
+import normal2 from './normal-2/index.js';
+import originalNew from './original-new/index.js';
+import originalOld from './original-old/index.js';
 import rule34 from './rule34/index.js';
+import shimmie2 from './shimmie2/index.js';
+import sketch1 from './sketch-1/index.js';
+import sketch2 from './sketch-2/index.js';
+import yousaLing from './yousa-ling/index.js';
 
 const themes = {
+  '3d-num': _3dNum,
+  'ai-1': ai1,
   asoul,
+  'booru-ffsr': booruFfsr,
+  'booru-helltaker': booruHelltaker,
+  'booru-huggboo': booruHuggboo,
+  'booru-jaypee': booruJaypee,
+  'booru-koe': booruKoe,
+  'booru-lewd': booruLewd,
+  'booru-lisu': booruLisu,
+  'booru-mjg': booruMjg,
+  'booru-mof': booruMof,
+  'booru-nandroid': booruNandroid,
+  'booru-qualityhentais': booruQualityhentais,
+  'booru-r6gdrawfriends': booruR6gdrawfriends,
+  'booru-rfck': booruRfck,
+  'booru-smtg': booruSmtg,
+  'booru-snyde': booruSnyde,
+  'booru-the-collection': booruTheCollection,
+  'booru-touhoulat': booruTouhoulat,
+  'booru-townofgravityfalls': booruTownofgravityfalls,
+  'booru-twifanartsfw': booruTwifanartsfw,
+  'booru-ve': booruVe,
+  'booru-vivi': booruVivi,
+  'booru-vp': booruVp,
+  'booru-yuyuyui': booruYuyuyui,
+  'capoo-1': capoo1,
+  'capoo-2': capoo2,
+  e621,
+  food,
   gelbooru,
   'gelbooru-h': gelbooruH,
+  green,
+  'kasuterura-1': kasuterura1,
+  'kasuterura-2': kasuterura2,
+  'kasuterura-3': kasuterura3,
+  'kasuterura-4': kasuterura4,
+  kyun,
+  'love-and-deepspace': loveAndDeepspace,
+  miku,
+  minecraft,
   moebooru,
   'moebooru-h': moebooruH,
+  'morden-num': mordenNum,
+  'nixietube-1': nixietube1,
+  'nixietube-2': nixietube2,
+  'normal-1': normal1,
+  'normal-2': normal2,
+  'original-new': originalNew,
+  'original-old': originalOld,
   rule34,
+  shimmie2,
+  'sketch-1': sketch1,
+  'sketch-2': sketch2,
+  'yousa-ling': yousaLing,
 };
 
 export default themes;
---
+++ themes/morden-num/index.js
@@ -0,0 +1,16 @@
+import image0 from './0.gif';
+import image1 from './1.gif';
+import image2 from './2.gif';
+import image3 from './3.gif';
+import image4 from './4.gif';
+import image5 from './5.gif';
+import image6 from './6.gif';
+import image7 from './7.gif';
+import image8 from './8.gif';
+import image9 from './9.gif';
+
+export default {
+  width: 54,
+  height: 52,
+  images: [image0, image1, image2, image3, image4, image5, image6, image7, image8, image9],
+};
